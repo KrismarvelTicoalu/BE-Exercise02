904d85da46b7bd1ff883a516d24e4db6b2c38752
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2024-01-30 11:08:07 +0800

    add 2 more members in members.js

--- members.js
+++ members.js
@@ -5,6 +5,18 @@ const members = [
     nama: "Ticoalu, Krismarvel Bryl Yitro",
     nomorTelp: "0895804040696",
   },
+  {
+    id: 2,
+    nim: "105022110081",
+    nama: "Moonik, Anjeli Saskia",
+    nomorTelp: "085825529340",
+  },
+  {
+    id: 3,
+    nim: "105022210001",
+    nama: "Siregar, Hizkia Gaynell Pascal",
+    nomorTelp: "081347170494",
+  },
 ];
 
-module.exports = members;
+Name: NIM: module.exports = members;
