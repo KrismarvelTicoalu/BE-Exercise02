08c2911150013dad42e10603b9ffecb77fef2206
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2024-02-12 19:54:17 +0800

    finished

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const members = require("./members");
 const users = require("./users");
 const moment = require("moment");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => res.send("This is the home page"));
 
@@ -25,29 +28,3 @@ app.get("/users", (req, res) => {
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`)
 );
-// const server = http
-//   .createServer((req, res) => {
-//     res.statusCode = 200;
-//     const url = req.url;
-
-//     if (url === "/") {
-//       res.setHeader("Content-Type", "text/plain");
-//       res.write("This is the home page");
-//     } else if (url === "/about") {
-//       res.setHeader("Content-Type", "text/json");
-//       res.write(
-//         JSON.stringify({
-//           Status: "success",
-//           Message: "response success",
-//           Description: "Exercise #02",
-//           Date: moment().format(),
-//           Data: members,
-//         })
-//       );
-//     } else if (url === "/users") {
-//       res.setHeader("Content-Type", "text/json");
-//       res.write(JSON.stringify(users));
-//     }
-//     res.end();
-//   })
-//   .listen(3000);
